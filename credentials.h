417a0290fb79588ea3f2fc0848cb914826ed15c3
Andy Wakefield <awakephd@gmail.com>
2024-10-08 09:32:02 -0400

    Setting up .gitignore to protect privacy of credentials.h

--- credentials.h
+++
@@ -1,7 +0,0 @@
-#ifndef CREDENTIALS_H
-#define CREDENTIALS_H
-
-const char WIFI_SSID[] =  "MySSID";
-const char WIFI_PSK[] =   "MyPSK"; 
-
-#endif
---
+++ credentials_format.h
@@ -0,0 +1,12 @@
+/* There must be a credentials.h file in the build
+   which will hold the WiFi credentials. The file
+   should look like the below:
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char WIFI_SSID[] =  "MySSID";
+const char WIFI_PSK[] =   "MyPSK"; 
+
+#endif
+*/
